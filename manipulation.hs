59d528add4b09a7db59895cf14ce1cf6a19ad9af
Timothy Wu <WuTP20@pwcs-edu.org>
2019-05-21 11:31:54 -0400

    almao

--- manipulation.hs
+++ manipulation.hs
@@ -1,1 +1,7 @@
-haskell
+reverseNum :: String -> Integer -> String
+reverseNum str num
+ | num `mod` 2 == 0 = str
+ | otherwise = reverse str
+
+boustrophedon :: [String] -> [String]
+boustrophedon list = [ reverseNum str x | str <- list, x <-[0..length list] ]
