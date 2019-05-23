994abef1bcdd4cce32a745264545cd6e5b242f7c
TomWoah123 <46789154+TomWoah123@users.noreply.github.com>
2019-05-23 10:38:43 -0400

    Fixed the errors

--- TaskTester.java
+++ TaskTester.java
@@ -1,19 +1,20 @@
 import java.util.*;
-public class TaskTester {
-
-    public static void main(String[] args) {
+public class TaskTester 
+{
+    public static void main(String[] args) 
+    {
         Scanner s = new Scanner( System.in );
         System.out.println( "Enter your name: ");
         String name = s.nextLine();
         System.out.println( "Your new name is: " + reverse(name) );
     }
     
-    public static String reverse(String name) {
+    public static String reverse(String name) 
+    {
         String boi = "";
         for ( int x = name.length() - 1; x >= 0; x-- ) {
             boi += name.charAt(x);
         }
         return boi;
     }
-
 }
