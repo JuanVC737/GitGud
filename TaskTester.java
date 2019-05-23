aae01198a35f9886ea44bd04aa1f75bd36a9e40d
JuanVC737 <40326734+JuanVC737@users.noreply.github.com>
2019-05-23 10:29:54 -0400

    Add files via upload

---
+++ TaskTester.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class TaskTester {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner( System.in );
+        System.out.println( "Enter your name: ");
+        String name = s.nextLine();
+        System.out.println( "Your new name is: " + reverse(name) );
+    }
+    
+    public static String reverse(String name) {
+        String boi = "";
+        for ( int x = name.length() - 1; x >= 0; x-- ) {
+            boi += name.charAt(x);
+        }
+        return boi;
+    }
+
+}
